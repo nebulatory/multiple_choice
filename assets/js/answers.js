14bb2adb8a02ec714dbbb085dc5439d6aa0f5675
d0ragon <asatianivaja@gmail.com>
2017-10-16 17:35:59 +0400

    Integrate Drupal.behaviors and jQuery.once into the javascript

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -1,76 +1,87 @@
-+function ($) {
++function ($, Drupal) {
   'use strict';
+  Drupal.behaviors.multipleChoiceBehavior = {
+    attach: function (context, settings) {
 
-  // .mcdw = multiple choice default widget
+      // .mcdw = multiple choice default widget
 
-  // remove an answer field
-  $('.mcdw-wrapper').on('click', '.mcdw-remove', function () {
-    var $wrapper = $(this).closest('.mcdw-answer-wrapper');
-    $wrapper
-      .find('.mcdw-checkbox, .mcdw-answer')
-      .attr('disabled', true)
-      .end()
-      .find('.mcdw-removed')
-      .val(1)
-      .end()
-      .find('.mcdw-undo')
-      .closest('.mcdw-undo-wrapper')
-      .removeClass('hidden')
-      .end()
-      .end()
-      .find('.mcdw-remove-wrapper')
-      .addClass('hidden');
+      // remove an answer field
+      $('.mcdw-wrapper', context).once('mcdwRemoveBehavior').each(function () {
+        $(this).on('click', '.mcdw-remove', function () {
+          var $wrapper = $(this).closest('.mcdw-answer-wrapper');
+          $wrapper
+            .find('.mcdw-checkbox, .mcdw-answer')
+            .attr('disabled', true)
+            .end()
+            .find('.mcdw-removed')
+            .val(1)
+            .end()
+            .find('.mcdw-undo')
+            .closest('.mcdw-undo-wrapper')
+            .removeClass('hidden')
+            .end()
+            .end()
+            .find('.mcdw-remove-wrapper')
+            .addClass('hidden');
 
-    return false;
-  });
+          return false;
+        });
+      });
 
-  // undo remove handler
-  $('.mcdw-wrapper').on('click', '.mcdw-undo', function () {
-    var $wrapper = $(this).closest('.mcdw-answer-wrapper');
-    $wrapper
-      .find('.mcdw-checkbox, .mcdw-answer')
-      .attr('disabled', false)
-      .end()
-      .find('.mcdw-removed')
-      .val(0)
-      .end()
-      .find('.mcdw-undo')
-      .closest('.mcdw-undo-wrapper')
-      .addClass('hidden')
-      .end()
-      .end()
-      .find('.mcdw-remove-wrapper')
-      .removeClass('hidden');
+      // undo remove handler
+      $('.mcdw-wrapper', context).once('mcdwUndoBehavior').each(function () {
+        $(this).on('click', '.mcdw-undo', function () {
+          var $wrapper = $(this).closest('.mcdw-answer-wrapper');
+          $wrapper
+            .find('.mcdw-checkbox, .mcdw-answer')
+            .attr('disabled', false)
+            .end()
+            .find('.mcdw-removed')
+            .val(0)
+            .end()
+            .find('.mcdw-undo')
+            .closest('.mcdw-undo-wrapper')
+            .addClass('hidden')
+            .end()
+            .end()
+            .find('.mcdw-remove-wrapper')
+            .removeClass('hidden');
 
-    return false;
-  });
+          return false;
+        });
+      });
 
-  // add answer handler
-  $('.mcdw-wrapper').on('click', '.mcdw-add', function () {
-    var $el = $('.mcdw-answer-wrapper'),
-        $parent = $el.parent(),
-        $clone = $el.eq(0).clone(),
-        index = $el.length;
+      // add answer handler
+      $('.mcdw-wrapper', context).once('mcdwAddBehaviour').each(function () {
+        $(this).on('click', '.mcdw-add', function () {
+          var $el = $(this).closest('.mcdw-wrapper').find('.mcdw-answer-wrapper').eq(0),
+              $parent = $el.parent(),
+              $clone = $el.clone(),
+              index = $el.siblings().length + 1;
 
-    var html = $('<div>')
-      .html($clone)
-      .html()
-      .replace(/(mcdw.{1,2}answers.{1,2})[0-9]+?/g, '\$1' + index);
-    $parent.append(html);
+          var html = $('<div>')
+            .html($clone)
+            .html()
+            .replace(/(mcdw.{1,2}answers.{1,2})[0-9]+?/g, '\$1' + index);
+          $parent.append(html);
 
-    $parent
-      .children()
-      .last()
-      .find(':checkbox')
-      .prop('checked', false)
-      .end()
-      .find('.mcdw-undo')
-      .click()
-      .end()
-      .find('.mcdw-answer')
-      .val('');
+          $parent
+            .children()
+            .last()
+            .find(':checkbox')
+            .prop('checked', false)
+            .end()
+            .find('.mcdw-undo')
+            .click()
+            .end()
+            .find('.mcdw-answer')
+            .val('');
 
-    return false;
-  });
+          return false;
+        });
+      });
 
-}(jQuery);
+    },
+  };
+
+}(jQuery, Drupal);
